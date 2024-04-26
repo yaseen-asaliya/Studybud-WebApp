2736a382544dbb30c464e3f7aeee91fbedf584a6
Yaseen Asaliya <yaseen-asaliya>
2024-04-26 20:13:47 +0300

    CRUD operations

--- base/views.py
+++ base/views.py
@@ -6,8 +6,6 @@ from .form import RoomForm
 
 def home(request):
     rooms = Room.objects.all()
-    for x in rooms:
-        print(x.created)
 
     context = {'rooms': rooms}
     return render(request, 'base/home.html' , context)
@@ -30,3 +28,24 @@ def createRoom(request):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == "POST":
+        room.delete()
+        return redirect('home')
+
+    return render(request, 'base/delete.html', {'obj': room})
