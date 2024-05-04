c14bfc2fcc2c17d016d886ddefee0436f647c4da
Yaseen Asaliya <yaseen-asaliya>
2024-05-04 16:02:18 +0300

    Search Box

--- base/views.py
+++ base/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .form import RoomForm
 
 # Create your views here.
 
 def home(request):
-    rooms = Room.objects.all()
+    # get q value from the url
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
 
-    context = {'rooms': rooms}
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+
     return render(request, 'base/home.html' , context)
 
 def room(request, pk):
@@ -49,3 +61,4 @@ def deleteRoom(request, pk):
         return redirect('home')
 
     return render(request, 'base/delete.html', {'obj': room})
+
